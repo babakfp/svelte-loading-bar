15625d32b2a8a90470148cd4d737f0c394ebec14
babakfp <44144724+babakfp@users.noreply.github.com>
2023-02-28 12:57:34 -0800

    renamed classnames

--- src/package/LoadingBarUi.svelte
+++ src/package/LoadingBarUi.svelte
@@ -3,12 +3,12 @@
 	export let classLoadingBarTrain: string | null | undefined = null
 </script>
 
-<div class="loading-bar {classLoadingBar || ''}">
-	<div class="loading-bar__train {classLoadingBarTrain || ''}" />
+<div class="loadingbar {classLoadingBar || ''}">
+	<div class="loadingbar-train {classLoadingBarTrain || ''}" />
 </div>
 
 <style lang="postcss">
-	.loading-bar {
+	.loadingbar {
 		position: fixed;
 		top: 0;
 		right: 0;
@@ -19,18 +19,18 @@
 		overflow: hidden;
 		z-index: 999;
 		background-color: var(
-			--loading-bar-background-color,
+			--loadingbar-background-color,
 			rgb(255 255 255 / 40%)
 		);
 	}
-	.loading-bar__train {
+	.loadingbar-train {
 		background-color: var(
-			--loading-bar__train-background-color,
+			--loadingbar-train-background-color,
 			rgb(255 255 255)
 		);
 	}
-	.loading-bar__train::before,
-	.loading-bar__train::after {
+	.loadingbar-train::before,
+	.loadingbar-train::after {
 		content: "";
 		position: absolute;
 		top: 0;
@@ -38,27 +38,27 @@
 		bottom: 0;
 		will-change: left, right;
 		background-color: var(
-			--loading-bar-background-color,
+			--loadingbar-background-color,
 			rgb(255 255 255 / 80%)
 		);
 	}
-	.loading-bar__train::before {
-		animation: loading-bar__train 2100ms
+	.loadingbar-train::before {
+		animation: loadingbar-train-animation 2100ms
 			cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite;
 	}
-	.loading-bar__train::after {
-		animation: loading-bar__train-short 2100ms
+	.loadingbar-train::after {
+		animation: loadingbar-train-short-animation 2100ms
 			cubic-bezier(0.165, 0.84, 0.44, 1) infinite;
 		animation-delay: 1.15s;
 	}
 	/* prettier-ignore */
-	@keyframes loading-bar__train {
+	@keyframes loadingbar-train-animation {
 		0%   { left: -35%; right: 100%; }
 		60%  { left: 100%; right: -90%; }
 		100% { left: 100%; right: -90%; }
 	}
 	/* prettier-ignore */
-	@keyframes loading-bar__train-short {
+	@keyframes loadingbar-train-short-animation {
 		0%   { left: -200%; right: 100%; }
 		60%  { left: 107%;  right: -8%;  }
 		100% { left: 107%;  right: -8%;  }

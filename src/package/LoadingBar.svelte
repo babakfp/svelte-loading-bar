0c60b8f84cc444c122b5d8442915887bb20abd2d
babakfp <44144724+babakfp@users.noreply.github.com>
2023-02-28 13:41:54 -0800

    Stuff

--- src/package/LoadingBar.svelte
+++ src/package/LoadingBar.svelte
@@ -1,11 +1,12 @@
-<script>
+<script lang="ts">
 	import { navigating } from "$app/stores"
 	import LoadingBarUi from "./LoadingBarUi.svelte"
+
 	export let delay = 250
 	let delayedPreloading = false
 	$: setTimeout(() => (delayedPreloading = !!$navigating), delay)
 </script>
 
-<!-- {#if delayedPreloading} -->
-<LoadingBarUi />
-<!-- {/if} -->
+{#if delayedPreloading}
+	<LoadingBarUi />
+{/if}

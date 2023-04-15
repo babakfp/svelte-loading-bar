997076f87193e0a331574a2882db6bbdecceb49d
babakfp <44144724+babakfp@users.noreply.github.com>
2023-04-15 12:09:34 -0700

    Updated Prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -10,31 +10,13 @@ module.exports = {
 	bracketSameLine: false,
 	quoteProps: "consistent",
 	htmlWhitespaceSensitivity: "strict",
-	importOrder: [
-		"/^@/", // A regular expression that matches strings that start with a @ symbol
-		"^\\$app",
-		"^\\$env",
-		"^\\$service-worker",
-		"^\\$lib/server",
-		"^\\$lib/",
-		"^\\$(?![^\\/]*\\/)", // A regular expression that matches strings that start with the character $ and do not contain the forward slash /
-		"^\\$[^/]*\\/[^/]+", // A regular expression that matches strings that start with the character $ and contain at least one forward slash /
-		"^[./]",
-	],
 	pluginSearchDirs: ["."],
 	plugins: [
 		require("prettier-plugin-svelte"),
 		require("prettier-plugin-tailwindcss"),
-		require("@trivago/prettier-plugin-sort-imports"),
 	],
 	overrides: [
 		{ files: "*.svelte", options: { parser: "svelte" } },
-		{
-			files: "*.md",
-			options: {
-				tabWidth: 2,
-				singleQuote: true,
-			},
-		},
+		{ files: "*.md", options: { tabWidth: 2 } },
 	],
 }

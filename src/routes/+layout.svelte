8ab968efe6c21629e2fe71bdeabe2378bb5127a5
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-06-10 08:41:21 +0330

    Added favicon and favicon by text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
     import "../app.postcss"
 </script>
 
-<slot />
+<main class="min-h-screen p-8">
+    <slot />
+</main>
+
+<div class="self-end border-t border-gray-800 py-4 text-center">
+    <p class="text-sm">
+        Favicon by
+        <a class="underline" href="https://icons8.com" target="_blank">Icon8</a
+        >.
+    </p>
+</div>
